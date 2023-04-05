9233345f8becb38c808cbfebc5f860b8c65d732a
Naveen Choudhary <naveenchoudhary129@gmail.com>
2023-04-05 21:23:47 +0530

    NavBar part done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+
 import Header from './components/header/Header';
+import Home from './components/home/Home';
+
+import { Box } from '@mui/material';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Header />
+      <Box style={{marginTop : 55}}>
+        <Home />
+      </Box>  
     </div>
   );
 }
